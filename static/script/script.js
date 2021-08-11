162456bebc2b97b8c6eacb7d65c489965e26ebf0
Mina Shoaib Rahman <shoaibsaikat@gmail.com>
2021-08-11 16:45:39 +0600

    empty functions

--- static/script/script.js
+++ static/script/script.js
@@ -8,3 +8,21 @@ function onChanged() {
         document.body.classList.add("bg-cyan");
     }
 }
+
+// function getWorkExperience() {
+//     var today = new Date();
+//     var todayString = (today.getMonth() + 1) + '/' + today.getDate() + '/' + today.getFullYear();
+//     var todayDate = new Date(todayString);
+    
+//     var joiningDate = new Date('03/18/2012');
+    
+//     var differenceInTime = todayDate.getTime() - joiningDate.getTime();
+//     var differenceInDays = differenceInTime / (1000 * 3600 * 24);
+//     return ((differenceInDays / 365) - 1).toFixed(1);
+// }
+
+// function getYearPassed(date1, date2) {   
+//     var differenceInTime = date2.getTime() - date1.getTime();
+//     var differenceInDays = differenceInTime / (1000 * 3600 * 24);
+//     return ((differenceInDays / 365)).toFixed(1);
+// }
