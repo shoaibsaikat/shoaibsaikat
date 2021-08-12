9e71c6da5176b4d078dea9f636f969f7dc2d82e4
Mina Shoaib Rahman <shoaibsaikat@gmail.com>
2021-08-12 14:35:02 +0600

    style change

--- static/script/script.js
+++ static/script/script.js
@@ -3,26 +3,22 @@ function onChanged() {
     if (toggleButton.checked) {
         document.body.classList.remove("bg-cyan");
         document.body.classList.add("bg-dark");
+
+        document.getElementById('about-me').classList.remove('bg-cyan', 'text-dark');
+        document.getElementById('about-me').classList.add('bg-dark', 'text-white-50');
+
+        document.getElementById('career-history').classList.remove('bg-cyan', 'text-dark');
+        document.getElementById('career-history').classList.add('bg-dark', 'text-white-50');
+
+
     } else {
         document.body.classList.remove("bg-dark");
         document.body.classList.add("bg-cyan");
+
+        document.getElementById('about-me').classList.remove('bg-dark', 'text-white-50');
+        document.getElementById('about-me').classList.add('bg-cyan', 'text-dark');
+
+        document.getElementById('career-history').classList.remove('bg-dark', 'text-white-50');
+        document.getElementById('career-history').classList.add('bg-cyan', 'text-dark');
     }
 }
-
-// function getWorkExperience() {
-//     var today = new Date();
-//     var todayString = (today.getMonth() + 1) + '/' + today.getDate() + '/' + today.getFullYear();
-//     var todayDate = new Date(todayString);
-    
-//     var joiningDate = new Date('03/18/2012');
-    
-//     var differenceInTime = todayDate.getTime() - joiningDate.getTime();
-//     var differenceInDays = differenceInTime / (1000 * 3600 * 24);
-//     return ((differenceInDays / 365) - 1).toFixed(1);
-// }
-
-// function getYearPassed(date1, date2) {   
-//     var differenceInTime = date2.getTime() - date1.getTime();
-//     var differenceInDays = differenceInTime / (1000 * 3600 * 24);
-//     return ((differenceInDays / 365)).toFixed(1);
-// }
