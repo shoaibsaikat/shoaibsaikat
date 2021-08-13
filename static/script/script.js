a3284fc8e1f43f0a228ec300b4d17eacda63e9e5
Mina Shoaib Rahman <shoaibsaikat@gmail.com>
2021-08-13 11:21:48 +0600

    style css change

--- static/script/script.js
+++ static/script/script.js
@@ -1,47 +1,42 @@
 function onChanged() {
     var toggleButton = document.getElementById("flexSwitch");
     if (toggleButton.checked) {
-
+        // dark
         document.getElementById('mode-name').innerHTML = "Light";
 
-        document.body.classList.remove("bg-cyan");
+        document.body.classList.remove("bg-secondary", "bg-opacity-10");
         document.body.classList.add("bg-dark");
 
-        document.getElementById('about-me').classList.remove('bg-cyan', 'text-dark');
+        document.getElementById('about-me').classList.remove('bg-secondary', 'bg-opacity-10', 'text-dark');
         document.getElementById('about-me').classList.add('bg-dark', 'text-white-50');
 
-        document.getElementById('career-history').classList.remove('bg-cyan', 'text-dark');
+        document.getElementById('career-history').classList.remove('bg-secondary', 'bg-opacity-10', 'text-dark');
         document.getElementById('career-history').classList.add('bg-dark', 'text-white-50');
 
-        document.getElementById('my-certification').classList.remove('bg-cyan', 'text-dark');
+        document.getElementById('my-certification').classList.remove('bg-secondary', 'bg-opacity-10', 'text-dark');
         document.getElementById('my-certification').classList.add('bg-dark', 'text-white-50');
 
-        document.getElementById('copy-right').classList.remove('bg-secondary', 'text-white');
-        document.getElementById('copy-right').classList.add('bg-cyan', 'text-dark');
-
-        // document.getElementById('theme').classList.remove('text-dark');
-        // document.getElementById('theme').classList.add('text-white');
+        document.getElementById('copyright').classList.remove('bg-secondary', 'text-light');
+        document.getElementById('copyright').classList.add('bg-light', 'text-dark');
 
 
     } else {
+        // light
         document.getElementById('mode-name').innerHTML = "Dark";
 
         document.body.classList.remove("bg-dark");
-        document.body.classList.add("bg-cyan");
+        document.body.classList.add("bg-secondary", "bg-opacity-10");
 
         document.getElementById('about-me').classList.remove('bg-dark', 'text-white-50');
-        document.getElementById('about-me').classList.add('bg-cyan', 'text-dark');
+        document.getElementById('about-me').classList.add('bg-secondary', 'bg-opacity-10', 'text-dark');
 
         document.getElementById('career-history').classList.remove('bg-dark', 'text-white-50');
-        document.getElementById('career-history').classList.add('bg-cyan', 'text-dark');
+        document.getElementById('career-history').classList.add('bg-secondary', 'bg-opacity-10', 'text-dark');
 
         document.getElementById('my-certification').classList.remove('bg-dark', 'text-white-50');
-        document.getElementById('my-certification').classList.add('bg-cyan', 'text-dark');
+        document.getElementById('my-certification').classList.add('bg-secondary', 'bg-opacity-25', 'text-dark');
 
-        document.getElementById('copy-right').classList.remove('bg-cyan', 'text-dark');
-        document.getElementById('copy-right').classList.add('bg-secondary', 'text-white');
-
-        // document.getElementById('theme').classList.remove('text-white');
-        // document.getElementById('theme').classList.add('text-dark');
+        document.getElementById('copyright').classList.remove('bg-light', 'text-dark');
+        document.getElementById('copyright').classList.add('bg-secondary', 'text-light');
     }
 }
